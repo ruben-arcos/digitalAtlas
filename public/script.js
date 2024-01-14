fdaca12099a318b86ad5b41878cee160442fdae6
Ruben Arcos <ruben.arcos06@gmail.com>
2024-01-14 17:50:27 -0600

    worked on script.js to make app functional

---
+++ public/script.js
@@ -0,0 +1,75 @@
+document.getElementById("searchButton").addEventListener("click", () => {
+  const countryName = document.getElementById("countryInput").value;
+  fetchCountryData(countryName);
+  console.log(countryName);
+});
+
+async function fetchCountryData(countryName) {
+  const messageElement = document.getElementById("message");
+  const resultsElement = document.getElementById("results");
+
+  try {
+    const response = await fetch(`/search?country=${countryName}`);
+    const data = await response.json();
+    // console.log(data);
+    if (data.length === 1) {
+      displayCountryInfo(data[0], resultsElement);
+    } else {
+      displayCountryList(data, resultsElement);
+    }
+  } catch (error) {
+    console.log(error);
+    messageElement.textContent =
+      "No result found. Please check your spelling and try again. This tool is built to search for the names of countries in English only.";
+  }
+}
+
+function displayCountryInfo(countryData, container) {
+  const languages = Object.values(countryData.languages).join(", ");
+  const currencyCode = Object.keys(countryData.currencies)[0];
+  const currency = countryData.currencies[currencyCode];
+  const currencyName = currency.name;
+  const currencySymbol = currency.symbol;
+
+  const demonym = countryData.demonyms?.eng
+    ? `${countryData.demonyms.eng.m}`
+    : "Not available";
+
+  container.innerHTML = `
+    <div class='country-name'>${countryData.name.common}</div>
+    <div class='official-name'>${countryData.name.official}</div>
+    <p><strong>Capital:</strong> ${
+      countryData.capital ? countryData.capital.join(", ") : "Not available"
+    }</p>
+    <p><strong>Region:</strong>${countryData.name.region}</p>
+    <p><strong>Population:</strong>${countryData.population.toLocaleString()}</p>
+    <p><strong>Languages:</strong>${languages}</p>
+    <p><strong>Currency:</strong>${currencyName}(${currencySymbol})</p>
+    <p><strong>Demonym:</strong> ${demonym}</p>
+    <p><strong>Independent:</strong> ${
+      countryData.independent ? "Yes" : "No"
+    }</p>
+    <p><strong>UN Member:</strong>${countryData.unMember ? "Yes" : "No"}<p>
+    <p><strong>Google Map:</strong><a href=${
+      countryData.maps.googleMaps
+    }>LINK</a></p>
+    `;
+}
+
+// function displayCountryInfo(countryData, container) {
+//     const languages = Object.values(countryData.languages).join(', ');
+
+//     container.innerHTML = `
+//         <div class='country-name'>${countryData.name.common}</div>
+//         <div class='official-name'>${countryData.name.official}</div>
+//         <p><strong>Capital:</strong> ${countryData.capital ? countryData.capital.join(", ") : "Not available" }</p>
+//         <p><strong>Region:</strong>${countryData.region}</p>
+//         <p><strong>Population:</strong>${countryData.population.toLocaleString()}</p>
+//         <p><strong>Languages:</strong>${languages}</p>
+//         <p><strong>Currency:</strong>${languages}</p>
+//         <p><strong>Demonym:</strong> <!-- Provide value or remove this line --></p>
+//         <p><strong>Independent:</strong> <!-- Provide value or remove this line --></p>
+//         <p><strong>UN Member:</strong></p>
+//         <p><strong>Google Map:</strong> <!-- Add link or information related to Google Map --></p>
+//     `;
+// }
