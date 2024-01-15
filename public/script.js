308835e97799e0b72e381a9c5cb3b1f155f872c3
Ruben Arcos <ruben.arcos06@gmail.com>
2024-01-14 18:25:24 -0600

    finished display country info function

--- public/script.js
+++ public/script.js
@@ -25,15 +25,32 @@ async function fetchCountryData(countryName) {
 }
 
 function displayCountryInfo(countryData, container) {
+  const flagElement = document.getElementById("flagContainer");
+  const armsElement = document.getElementById("coatOfArmsContainer");
+
   const languages = Object.values(countryData.languages).join(", ");
   const currencyCode = Object.keys(countryData.currencies)[0];
   const currency = countryData.currencies[currencyCode];
   const currencyName = currency.name;
   const currencySymbol = currency.symbol;
 
+  const flagUrl = countryData.flags.png;
+  const flagAlt = countryData.flags.alt
+    ? countryData.flags.alt
+    : `Flag of ${countryData.name.common}`;
+  const coatOfArmsUrl = countryData.coatOfArms.png;
+
   const demonym = countryData.demonyms?.eng
     ? `${countryData.demonyms.eng.m}`
     : "Not available";
+
+  if (flagUrl) {
+    flagElement.style.display = "block";
+  }
+
+  if (coatOfArmsUrl) {
+    armsElement.style.display = "block";
+  }
 
   container.innerHTML = `
     <div class='country-name'>${countryData.name.common}</div>
@@ -54,22 +71,14 @@ function displayCountryInfo(countryData, container) {
       countryData.maps.googleMaps
     }>LINK</a></p>
     `;
+
+  container.document.getElementById("flagContainer").innerHTML = flagUrl
+    ? `<p><strong>Flag: </strong></p><img src="${flagUrl}" alt="${flagAlt}">`
+    : "";
+
+  document.getElementById("coatOfArmsContainer").innerHTML = flagUrl
+    ? `<p><strong>Coat of Arms: </strong></p><img src="${coatOfArmsUrl}" alt="Coat of Arms of ${countryData.name.common}">`
+    : "";
 }
 
-// function displayCountryInfo(countryData, container) {
-//     const languages = Object.values(countryData.languages).join(', ');
 
-//     container.innerHTML = `
-//         <div class='country-name'>${countryData.name.common}</div>
-//         <div class='official-name'>${countryData.name.official}</div>
-//         <p><strong>Capital:</strong> ${countryData.capital ? countryData.capital.join(", ") : "Not available" }</p>
-//         <p><strong>Region:</strong>${countryData.region}</p>
-//         <p><strong>Population:</strong>${countryData.population.toLocaleString()}</p>
-//         <p><strong>Languages:</strong>${languages}</p>
-//         <p><strong>Currency:</strong>${languages}</p>
-//         <p><strong>Demonym:</strong> <!-- Provide value or remove this line --></p>
-//         <p><strong>Independent:</strong> <!-- Provide value or remove this line --></p>
-//         <p><strong>UN Member:</strong></p>
-//         <p><strong>Google Map:</strong> <!-- Add link or information related to Google Map --></p>
-//     `;
-// }
